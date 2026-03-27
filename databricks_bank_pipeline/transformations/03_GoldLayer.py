c7152be9632c488ab421109cf9ef7ca16e544c4c
Manu-RMT <mandu543@gmail.com>
2026-03-27 09:38:58 +0000

    Agg Materialized Viex to GoldLayer OK

--- databricks_bank_pipeline/transformations/03_GoldLayer.py
+++ databricks_bank_pipeline/transformations/03_GoldLayer.py
@@ -17,3 +17,39 @@ def bank_gold_cust_acc_trns_mv():
                                 how="inner"
                                 )
   return joined
+
+
+#######  AGGREGATION for the trades #######
+@dlt.table(
+    name = f"{GOLD_ZONE}.bank_gold_cust_acc_trns_agg",
+    comment = "Gold Layer aggregation of customers + account transactions"
+)
+
+def bank_gold_cust_acc_trns_agg():
+    df = dlt.read(f"{GOLD_ZONE}.bank_gold_cust_acc_trns_mv")
+
+    return(
+        
+        df.groupBy("customer_id", "name", "gender", "city", "status", "income_range","risk_segment","customer_age","tenure_months","tenure_years")
+          .agg(
+              countDistinct("account_id").alias("account_count"),
+              count("*").alias("txn_count"),
+              round(
+                  sum(when(
+                        col("txn_type") == "CREDIT", col("txn_amount")
+                      ).otherwise(lit(0.0))
+                  )
+                  ,2).alias("total_credit"),
+              round(
+                  sum(when(
+                        col("txn_type") == "DEBIT", col("txn_amount")
+                       ).otherwise(lit(0.0))
+                  )
+                  ,2).alias("total_debit"),
+              round(avg(col("txn_amount")),2).alias("avg_txn_amount"),
+              min("txn_date").alias("first_txn_date"),
+              max("txn_date").alias("last_txn_date"),
+              countDistinct("txn_channel").alias("channels_used")
+          )         
+    )
+     
